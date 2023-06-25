3f3831d8aaff2c1e8bd6b519349841f8bce6cde8
huy <huy@gmail.com>
2023-06-25 22:07:53 +0700

    huydev

---
+++ src/pages/products/List.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Layout from '../../libs/Layout'
+
+export default function List() {
+  return (
+    <Layout>List</Layout>
+  )
+}
--- src/routes/Web.jsx
+++ src/routes/Web.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import NotFound from "../pages/NotFound";
-import List from "../pages/categories/List";
+import ListCategory from "../pages/categories/List";
+import ListProduct from "../pages/products/List";
 
 export default function Web() {
   return (
     <Routes>
       <Route path="/" exact element={<Home />} />
-      <Route path="/categories" element={<List />} />
+      <Route path="/categories" element={<ListCategory />} />
+      <Route path="/products" element={<ListProduct />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
