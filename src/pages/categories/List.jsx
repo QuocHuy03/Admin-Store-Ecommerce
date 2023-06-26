f34c0bd576a8f805b3debbe1d2479ef0fd2ef774
huy <huy@gmail.com>
2023-06-26 17:13:24 +0700

    huydev

--- src/pages/categories/List.jsx
+++ src/pages/categories/List.jsx
@@ -6,6 +6,7 @@ import ModalForm from "../../components/Modal";
 import { useForm } from "react-hook-form";
 import { Button, Modal } from "antd";
 import { message } from "antd";
+import { LoadingOutlined } from "@ant-design/icons";
 import {
   fetchAllCategories,
   fetchDeleteCategory,
@@ -151,138 +152,108 @@ export default function List() {
         </div>
 
         <ModalForm
-          title={"Add Category"}
+          title={"Category"}
           isOpenModal={isOpenModal}
           onClose={closeModal}
+          footer={[
+            <Button key="cancel" onClick={closeModal}>
+              Cancel
+            </Button>,
+            <Button
+              key="ok"
+              type="primary"
+              style={{ background: "#1677ff" }}
+              onClick={handleSubmit(onSubmit)}
+              loading={isSubmitting}
+              icon={
+                isSubmitting ? (
+                  <LoadingOutlined style={{ color: "white" }} />
+                ) : null
+              }
+            >
+              OK
+            </Button>,
+          ]}
         >
           <form onSubmit={handleSubmit(onSubmit)}>
-            {/* Modal body */}
-            <div className="p-6 space-y-6">
-              <div className="grid gap-4 md:grid-cols-2">
-                <div>
-                  <label
-                    htmlFor="full_name"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  >
-                    Name Category
-                  </label>
-                  <input
-                    type="text"
-                    id="nameCategory"
-                    {...register("nameCategory", { required: true })}
-                    placeholder="Macbook ..."
-                    className={`${
-                      errors.nameCategory ? "border-red-500" : "border-gray-300"
-                    } bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
-                  />
-                  {errors.nameCategory && (
-                    <p className="text-red-500 text-sm mt-1">
-                      * Name category is required
-                    </p>
-                  )}
-                </div>
-
-                <div>
-                  <label
-                    htmlFor="statusCategory"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  >
-                    Status
-                  </label>
-
-                  <select
-                    id="statusCategory"
-                    className={`${
-                      errors.statusCategory
-                        ? "border-red-500"
-                        : "border-gray-300"
-                    } bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
-                    {...register("statusCategory", { required: true })}
-                    defaultValue="" // Update defaultValue attribute
-                  >
-                    <option value="">Vui Lòng Chọn Trạng Thái </option>
-                    <option value="stocking">Còn Hàng</option>
-                    <option value="out-of-stock">Hết Hàng</option>
-                  </select>
-                  {errors.statusCategory && (
-                    <p className="text-red-500 text-sm mt-1">
-                      * Status is required
-                    </p>
-                  )}
-                </div>
-                <div>
-                  <label
-                    htmlFor="outstanding"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  >
-                    Outstanding
-                  </label>
-
-                  <select
-                    id="outstandingCategory"
-                    className={`${
-                      errors.outstandingCategory
-                        ? "border-red-500"
-                        : "border-gray-300"
-                    } bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
-                    {...register("outstandingCategory", { required: true })}
-                    defaultValue=""
-                  >
-                    <option value="">Vui Lòng Chọn Sản Phẩm Nổi Bật</option>
-                    <option value="outstanding">Nổi Bật</option>
-                    <option value="notstandout">Không Nổi Bật</option>
-                  </select>
-                  {errors.outstandingCategory && (
-                    <p className="text-red-500 text-sm mt-1">
-                      * Outstanding is required
-                    </p>
-                  )}
-                </div>
+            <div className="grid gap-4 md:grid-cols-2">
+              <div>
+                <label
+                  htmlFor="nameCategory"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Name
+                </label>
+                <input
+                  type="text"
+                  id="nameCategory"
+                  {...register("nameCategory", { required: true })}
+                  placeholder="Macbook ..."
+                  className={`${
+                    errors.nameCategory ? "border-red-500" : "border-gray-300"
+                  } bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
+                />
+                {errors.nameCategory && (
+                  <p className="text-red-500 text-sm mt-1">
+                    * Name category is required
+                  </p>
+                )}
               </div>
-            </div>
 
-            {/* Modal footer */}
-            <div className="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
-              {isSubmitting ? (
-                <button
-                  disabled
-                  type="button"
-                  className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600 cursor-not-allowed "
+              <div>
+                <label
+                  htmlFor="statusCategory"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
-                  <svg
-                    aria-hidden="true"
-                    role="status"
-                    className="inline w-4 h-4 mr-3 text-white animate-spin"
-                    viewBox="0 0 100 101"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                      fill="#E5E7EB"
-                    />
-                    <path
-                      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                      fill="currentColor"
-                    />
-                  </svg>
-                  Loading
-                </button>
-              ) : (
-                <button
-                  type="submit"
-                  className=" text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
+                  Status
+                </label>
+
+                <select
+                  id="statusCategory"
+                  className={`${
+                    errors.statusCategory ? "border-red-500" : "border-gray-300"
+                  } bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
+                  {...register("statusCategory", { required: true })}
+                  defaultValue="" // Update defaultValue attribute
                 >
-                  Add
-                </button>
-              )}
-              <button
-                type="button"
-                onClick={closeModal}
-                className=" text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
-              >
-                Close
-              </button>
+                  <option value="">Vui Lòng Chọn Trạng Thái </option>
+                  <option value="stocking">Còn Hàng</option>
+                  <option value="out-of-stock">Hết Hàng</option>
+                </select>
+                {errors.statusCategory && (
+                  <p className="text-red-500 text-sm mt-1">
+                    * Status is required
+                  </p>
+                )}
+              </div>
+              <div>
+                <label
+                  htmlFor="outstanding"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Outstanding
+                </label>
+
+                <select
+                  id="outstandingCategory"
+                  className={`${
+                    errors.outstandingCategory
+                      ? "border-red-500"
+                      : "border-gray-300"
+                  } bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
+                  {...register("outstandingCategory", { required: true })}
+                  defaultValue=""
+                >
+                  <option value="">Vui Lòng Chọn Sản Phẩm Nổi Bật</option>
+                  <option value="outstanding">Nổi Bật</option>
+                  <option value="notstandout">Không Nổi Bật</option>
+                </select>
+                {errors.outstandingCategory && (
+                  <p className="text-red-500 text-sm mt-1">
+                    * Outstanding is required
+                  </p>
+                )}
+              </div>
             </div>
           </form>
         </ModalForm>
