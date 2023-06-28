40f15fbdf363ea038ba329806b544a7ff0eb3c06
huy <huy@gmail.com>
2023-06-28 10:02:41 +0700

    huydev

--- src/pages/categories/List.jsx
+++ src/pages/categories/List.jsx
@@ -22,8 +22,6 @@ export default function List() {
   const [dataIdToDelete, setDataIdToDelete] = useState(null);
   const [categoryName, setCategoryName] = useState("");
 
-  const [searchTerm, setSearchTerm] = useState("");
-
   const [paginate, setPaginate] = useState({
     page: paginateEnv.page,
     limit: paginateEnv.limit,
@@ -57,11 +55,14 @@ export default function List() {
     },
   });
 
-  const { data, isLoading, refetch } = useQuery({
-    queryKey: ["categories", , paginate.page, paginate.limit],
-    queryFn: () => fetchAllCategoriesPage(paginate.page, paginate.limit),
-    staleTime: 1000,
-  });
+  const { data, isLoading, refetch } = useQuery(
+    ["categories", paginate.page, paginate.limit],
+    () => fetchAllCategoriesPage(paginate.page, paginate.limit),
+    {
+      staleTime: 1000,
+      enabled: false, // Disable automatic fetching on component mount
+    }
+  );
 
   // panigate
 
@@ -74,30 +75,9 @@ export default function List() {
 
   useEffect(() => {
     refetch();
+    // return () => {
+    // };
   }, [paginate.page, paginate.limit, refetch]);
-
-  // filter table
-
-  const handleSearch = (event) => {
-    const searchTerm = event.target.value;
-    setSearchTerm(searchTerm);
-  };
-
-  useEffect(() => {
-    if (searchTerm) {
-      // Lọc dữ liệu dựa vào giá trị search
-      const filteredDataHuy = Array.isArray(data?.data)
-        ? data.data.filter((item) =>
-            item.nameCategory.toLowerCase().includes(searchTerm.toLowerCase())
-          )
-        : [];
-
-      data.data = filteredDataHuy;
-    } else {
-      queryClient.invalidateQueries("categories");
-    }
-  }, [searchTerm, data?.data]);
-
   // modal
 
   const queryClient = useQueryClient();
@@ -163,34 +143,6 @@ export default function List() {
     <Layout>
       <div className="relative overflow-x-auto">
         <div className="flex items-center justify-between pb-4">
-          <label htmlFor="table-search" className="sr-only">
-            Search
-          </label>
-          <div className="relative">
-            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-              <svg
-                className="w-5 h-5 text-gray-500 dark:text-gray-400"
-                aria-hidden="true"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </div>
-            <input
-              type="text"
-              id="table-search"
-              className="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Search for items"
-              value={searchTerm}
-              onChange={handleSearch}
-            />
-          </div>
           <button
             onClick={openModal}
             className=" text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
