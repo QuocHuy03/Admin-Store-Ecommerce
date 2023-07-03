0f5a64eac967c5a05de0384d643d9eedf8342595
huy <huy@gmail.com>
2023-07-03 22:13:34 +0700

    huydev

--- src/pages/products/Add.jsx
+++ src/pages/products/Add.jsx
@@ -16,7 +16,6 @@ export default function Add() {
 
   const postProductMutation = useMutation((data) => fetchPostProduct(data));
 
-
   const onSubmit = async (data) => {
     setIsSubmitting(true);
 
@@ -57,97 +56,143 @@ export default function Add() {
         <div className="grid gap-6 mb-6 md:grid-cols-2">
           <div>
             <label
-              htmlFor="first_name"
+              htmlFor="nameProduct"
               className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
             >
-              First name
+              Name Product
             </label>
+
             <input
               type="text"
-              id="first_name"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="John"
-              required
+              id="nameProduct"
+              className={`${
+                errors.nameProduct ? "border-red-500" : "border-gray-300"
+              } bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
+              {...register("nameProduct", { required: true })}
+              placeholder="Asus GB5 ..."
             />
+            {errors.nameProduct && (
+              <p className="text-red-500 text-sm mt-1">
+                * Name Product is required
+              </p>
+            )}
           </div>
           <div>
             <label
-              htmlFor="last_name"
+              htmlFor="price_has_ropped"
               className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
             >
-              Last name
-            </label>
-            <input
-              type="text"
-              id="last_name"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Doe"
-              required
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="company"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Company
-            </label>
-            <input
-              type="text"
-              id="company"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Flowbite"
-              required
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="phone"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Phone number
-            </label>
-            <input
-              type="tel"
-              id="phone"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="123-45-678"
-              pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
-              required
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="website"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Website URL
-            </label>
-            <input
-              type="url"
-              id="website"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="flowbite.com"
-              required
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="visitors"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Unique visitors (per month)
+              Giá Đã Giảm
             </label>
             <input
               type="number"
-              id="visitors"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder
-              required
+              id="price_has_ropped"
+              className={`${
+                errors.price_has_ropped ? "border-red-500" : "border-gray-300"
+              } bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
+              {...register("price_has_ropped", { required: true })}
+              placeholder="1000"
             />
+            {errors.price_has_ropped && (
+              <p className="text-red-500 text-sm mt-1">
+                * Price Has ropped is required
+              </p>
+            )}
+          </div>
+          <div>
+            <label
+              htmlFor="categoryID"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Categories
+            </label>
+            <select
+              id="categoryID"
+              className={`${
+                errors.categoryID ? "border-red-500" : "border-gray-300"
+              } bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
+              {...register("categoryID", { required: true })}
+            >
+              <option value="">Vui Lòng Chọn Trạng Thái</option>
+            </select>
+            {errors.categoryID && (
+              <p className="text-red-500 text-sm mt-1">
+                * Categories is required
+              </p>
+            )}
+          </div>
+          <div>
+            <label
+              htmlFor="initial_price"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Giá Ban Đầu
+            </label>
+            <input
+              type="number"
+              id="initial_price"
+              className={`${
+                errors.initial_price ? "border-red-500" : "border-gray-300"
+              } bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
+              {...register("initial_price", { required: true })}
+              placeholder="10000"
+            />
+            {errors.initial_price && (
+              <p className="text-red-500 text-sm mt-1">
+                * Initial price is required
+              </p>
+            )}
+          </div>
+          <div>
+            <label
+              htmlFor="contentProduct"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Nội Dung
+            </label>
+
+            <textarea
+              id="contentProduct"
+              rows="4"
+              className={`${
+                errors.contentProduct ? "border-red-500" : "border-gray-300"
+              } bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
+              {...register("contentProduct", { required: true })}
+              placeholder="Nội Dung"
+            ></textarea>
+
+            {errors.contentProduct && (
+              <p className="text-red-500 text-sm mt-1">
+                * Content Product is required
+              </p>
+            )}
+          </div>
+          <div>
+            <label
+              htmlFor="statusProduct"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Status
+            </label>
+
+            <select
+              id="statusProduct"
+              className={`${
+                errors.statusProduct ? "border-red-500" : "border-gray-300"
+              } bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
+              {...register("statusProduct", { required: true })}
+            >
+              <option value="">Vui Lòng Chọn Trạng Thái</option>
+              <option value="stocking">Còn Hàng</option>
+              <option value="out-of-stock">Hết Hàng</option>
+            </select>
+            {errors.statusProduct && (
+              <p className="text-red-500 text-sm mt-1">* Status is required</p>
+            )}
           </div>
         </div>
-        <div className="mb-6">
+
+        {/* <div className="mb-6">
           <label
             htmlFor="email"
             className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
@@ -161,61 +206,9 @@ export default function Add() {
             placeholder="john.doe@company.com"
             required
           />
-        </div>
-        <div className="mb-6">
-          <label
-            htmlFor="password"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-          >
-            Password
-          </label>
-          <input
-            type="password"
-            id="password"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="•••••••••"
-            required
-          />
-        </div>
-        <div className="mb-6">
-          <label
-            htmlFor="confirm_password"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-          >
-            Confirm password
-          </label>
-          <input
-            type="password"
-            id="confirm_password"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="•••••••••"
-            required
-          />
-        </div>
-        <div className="flex items-start mb-6">
-          <div className="flex items-center h-5">
-            <input
-              id="remember"
-              type="checkbox"
-              defaultValue
-              className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
-              required
-            />
-          </div>
-          <label
-            htmlFor="remember"
-            className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-          >
-            I agree with the{" "}
-            <a
-              href="#"
-              className="text-blue-600 hover:underline dark:text-blue-500"
-            >
-              terms and conditions
-            </a>
-            .
-          </label>
-        </div>
+        </div> */}
+     
+       
         <button
           type="submit"
           className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
